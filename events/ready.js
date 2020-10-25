3936d23c64c69ae1f082bfb9e04f21b3006d7f48
Bruna & Lrd <73443391+LRD6666@users.noreply.github.com>
2020-10-25 17:06:50 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,9 +4,9 @@ module.exports.run = (client) => {
   console.log("JOIN IDA SERVER : https://discord.gg/xBAYBY")
   let atividades = [
     `Type \`*help\` for commands!`,
-    `Bot created by LRD`,
-    `Only for betas`,
-    `Version: 0.1.5`
+    `Bot created by LRD & BRUNA`,
+    `Enjoy in my github ;) \`*github\``,
+    `Version: 0.2.5 Public`
   ],
     i = 0;
   setInterval(() => client.user.setActivity(`${atividades[i++ % atividades.length]}`, {
